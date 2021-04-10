85896997859d48d40c6c1520283d550fa244d269
bvmensvoort <info@bvmensvoort.nl>
2021-04-10 19:09:40 +0200

    Back to basics for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 set -eu
 
 
-if [[ -z $CARDDAV ]]; then sed -i '/<carddav>/,/<\/carddav>/g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template; fi
-sed -i 's/<carddav>//g;s/<\/carddav>//g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template;
+#if [[ -z $CARDDAV ]]; then sed -i '/<carddav>/,/<\/carddav>/g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template; fi
+#sed -i 's/<carddav>//g;s/<\/carddav>//g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template;
 
-if [[ -z $CALDAV ]]; then sed -i '/<caldav>/,/<\/caldav>/g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template; fi
-sed -i 's/<caldav>//g;s/<\/caldav>//g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template;
+#if [[ -z $CALDAV ]]; then sed -i '/<caldav>/,/<\/caldav>/g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template; fi
+#sed -i 's/<caldav>//g;s/<\/caldav>//g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.template;
 
 envsubst '$$PORT$$HOST_IP$$CERTIFICATE_FILENAME$$PRIVATEKEY_FILENAME$$DOMAIN$$DOMAINPORT$$CLIENTMAXBODYSIZE$$STRICTTRANSPORTSECURITY' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
